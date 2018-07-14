ea8ffec341ab0823f406b6057d572c7682979e3f
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-07-14 14:50:44 -0400

    Refactored index page

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    login_as(user, scope: :user)
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, scope: :user)
   end
 
   describe 'index' do
@@ -20,10 +20,10 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create!(date: Date.today, rationale: "Anything")
-      post2 = Post.create!(date: Date.today, rationale: "Anything")
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
       visit posts_path
-      expect(page).to have_content(/post1|post2/)
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
@@ -46,10 +46,10 @@ describe 'navigate' do
 
     it 'will have a user asscociated with it' do
       fill_in 'post[date]', with: Date.today
-      fill_in 'post[rationale]', with: "User_Association"
+      fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
 
-      expect(User.last.posts.last.rationale).to eq("User_Association")
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
